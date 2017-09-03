55daaac4533bff1be71ed090f816c076fae5fc48
LZAH <laith43d@gmail.com>
2017-09-03 11:45:47 +0300

    final project v01

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,9 +5,25 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage
+{
+  name1 = '';
+  name2 = '';
 
-  constructor(public navCtrl: NavController) {
+  get score()
+  {
+    const letters = (this.name1 + this.name2).toLowerCase();
+    let sum = 0;
+    for( let i = 0; i < letters.length; i++ )
+    {
+      sum += letters.charCodeAt(i);
+    }
+    return sum % 101;
+
+  }
+
+  constructor(public navCtrl: NavController)
+  {
 
   }
 
